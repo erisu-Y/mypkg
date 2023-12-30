dcc6165306db714c148f2b70109ecfab18c2959e
Yuto Okuda <s22c1024dh@s.chibakoudai.jp>
2023-12-30 16:53:29 +0900

    update setup.py & test/test.bash

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: ©2023 Yuto Okuda
+# SPDX-License-Identifier:BSD-3-Clause
 from setuptools import setup
 import os
 from glob import glob
--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: ©2023 Yuto Okuda
+# SPDX-License-Identifier:BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
